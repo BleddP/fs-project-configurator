222b3e9b079ec084d7c94bb8963ddc395708a528
BleddP <pijpersbleddyn@gmail.com>
2021-04-14 00:29:32 +0800

    fixed filter function

--- src/views/ConfiguratorView.vue
+++ src/views/ConfiguratorView.vue
@@ -18,145 +18,93 @@
 </template>
 
 <script>
-import ConfigIntro from '../components/interactive/configurator/ConfigIntro';
-import ConfigQuestions from '../components/interactive/configurator/ConfigQuestions';
-import ConfigResults from '../components/interactive/configurator/ConfigResults';
-import ConfigContact from '../components/interactive/configurator/ConfigContact';
-import axios from 'axios';
+import ConfigIntro from "../components/interactive/configurator/ConfigIntro";
+import ConfigQuestions from "../components/interactive/configurator/ConfigQuestions";
+import ConfigResults from "../components/interactive/configurator/ConfigResults";
+import ConfigContact from "../components/interactive/configurator/ConfigContact";
+import axios from "axios";
 
 export default {
-  name: 'ConfiguratorView',
+  name: "ConfiguratorView",
   components: {
     ConfigIntro,
     ConfigQuestions,
     ConfigResults,
-    ConfigContact,
+    ConfigContact
   },
   data() {
     return {
       products: [],
       productsArray: [],
-      type: '',
-      width: 5000,
-      height: 5000,
-      assemblyPosition: '',
+      type: null,
+      width: 0,
+      height: 0,
+      assemblyPosition: null,
       selfHealing: null,
       stainlessSteel: null,
-      windLoad: null,
+      windLoad: null
     };
   },
   methods: {
     selectDoorType(value) {
       this.type = value;
-      this.filterResults();
+      this.productsArray = filterResults(this);
     },
     selectWidth(value) {
       this.width = value;
-      this.filterResults();
+      this.productsArray = filterResults(this);
     },
     selectHeight(value) {
       this.height = value;
-      this.filterResults();
+      this.productsArray = filterResults(this);
     },
     selectAssembly(value) {
       this.assemblyPosition = value;
-      this.filterResults();
+      this.productsArray = filterResults(this);
     },
     selectSelfHealing(value) {
       this.selfHealing = value;
-      this.filterResults();
+      this.productsArray = filterResults(this);
     },
     selectStainlessSteel(value) {
       this.stainlessSteel = value;
-      this.filterResults();
+      this.productsArray = filterResults(this);
     },
     selectWindLoad(value) {
       this.windLoad = value;
-      this.filterResults();
-    },
-    filterResults() {
-      const {
-        type,
-        width,
-        height,
-        assemblyPosition,
-        selfHealing,
-        stainlessSteel,
-        windLoad,
-      } = this;
-
-      /// Filter the room by door type
-      let byDoorType;
-      if (type !== '') {
-        byDoorType = this.products.filter((door) => door.type === type);
-      } else {
-        byDoorType = this.products;
-      }
-
-      /// Filter the door by width
-      const byWidth = byDoorType.filter((door) => width < door.width);
-
-      /// Filter the door by height
-      const byHeight = byWidth.filter((door) => height < door.height);
-
-      /// Filter the door by assembly left/right
-      let byAssemblyPosition;
-      if (assemblyPosition !== '') {
-        byAssemblyPosition = byHeight.filter(
-          (door) => door.assemblyPosition === assemblyPosition
-        );
-      } else {
-        byAssemblyPosition = byHeight;
-      }
-
-      /// Filter the door by self-healing properties
-      let bySelfHealing;
-      if (selfHealing !== null) {
-        bySelfHealing = byAssemblyPosition.filter(
-          (door) => door.selfHealing === selfHealing
-        );
-      } else {
-        bySelfHealing = byAssemblyPosition;
-      }
-
-      /// Filter the door by stainless-steel
-      let byStainlessSteel;
-      if (stainlessSteel !== null) {
-        byStainlessSteel = bySelfHealing.filter(
-          (door) => door.stainlessSteel === stainlessSteel
-        );
-      } else {
-        byStainlessSteel = bySelfHealing;
-      }
-
-      /// Filter the door by windload
-      let byWindLoad;
-      if (windLoad !== 0) {
-        byWindLoad = byStainlessSteel.filter(
-          (door) => door.windLoad === windLoad
-        );
-      } else {
-        byWindLoad = byStainlessSteel;
-      }
-
-      const filteredResults = byWindLoad;
-      return (this.productsArray = filteredResults);
-    },
+      this.productsArray = filterResults(this);
+    }
   },
 
   // Get the initial data from a JSON file
   created() {
     axios
-      .get('/test_data/products.json')
-      .then((res) => {
+      .get("/test_data/products.json")
+      .then(res => {
         this.products = res.data.doorTypes;
         this.productsArray = res.data.doorTypes;
       })
-      .catch((err) => {
+      .catch(err => {
         console.log({ err });
       });
-  },
+  }
 };
+
+function filterResults(input) {
+  const filteredResults = input.products.filter(door => {
+    return (
+      (input.type === door.type || !input.type) &&
+      (input.width < door.width ?? input.type === 0) &&
+      (input.height < door.height ?? input.type === 0) &&
+      (input.assemblyPosition === door.assemblyPosition ||
+        !input.assemblyPosition) &&
+      (input.selfHealing === door.selfHealing || !input.selfHealing) &&
+      (input.stainlessSteel === door.stainlessSteel || !input.stainlessSteel) &&
+      (input.windLoad === door.windLoad || !input.windLoad)
+    );
+  });
+  return filteredResults;
+}
 </script>
 
 <style scoped></style>
